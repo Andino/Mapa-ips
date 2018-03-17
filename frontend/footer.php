bfee4e65a9e727c1b359211e711eb62ca93c47dc
Melara22 <melara2396@gmail.com>
2018-03-17 00:31:31 -0600

    cambios fuck my head

--- frontend/footer.php
+++ frontend/footer.php
@@ -14,6 +14,80 @@ jQuery(document).ready(function($){
 });
 
 </script>
+<script type="text/javascript">
+	
+	$('#depa').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa1').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa2').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+
+	$('#depa3').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa4').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa5').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa6').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa7').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa8').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa9').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa10').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa11').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa12').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})
+	$('#depa13').click(function(e) {      
+    location.href =  $(this).attr('data-href');
+    e.preventDefault();
+
+})	
+</script>
 </body>
 <footer>
   <p class="temp-footer">Fundación Poma, impulsando el Progreso Social | Todos los derechos reservados 2017</p>
